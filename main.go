563ebe906968184d2c04cca76a5922a11d5859f1
stephanustedy <stephanus.tedy@gmail.com>
2019-01-03 17:53:54 +0700

    changing hello world text

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello world")
+		fmt.Fprintf(w, "hello world 2")
 	})
 
 	http.ListenAndServe(":1234", nil)
