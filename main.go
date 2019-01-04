9d73d64569780b4767f86d96b6df21bc48035832
stephanustedy <stephanus.tedy@gmail.com>
2019-01-04 14:09:25 +0700

    Adding 1 more load balancer

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello world 3")
+	})
+
+	http.HandleFunc("/panic", func(w http.ResponseWriter, r *http.Request) {
+		log.Fatal(w, "hello world 3")
 	})
 
 	http.ListenAndServe(":1234", nil)
